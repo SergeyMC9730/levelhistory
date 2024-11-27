2dcfb3dc3b354a526ff135158dae18b3ccfa5365
Sergei Baigerov <bdsservers@yandex.ru>
2024-11-27 18:16:23 +0800

    nonono wrong fix

--- src/ProviderPopup.cpp
+++ src/ProviderPopup.cpp
@@ -1502,12 +1502,14 @@ void ProviderPopup::removeThumbnailForCell(LevelCell *cell) {
             continue;
         }
 
-        auto as_clip = typeinfo_cast<CCNode *>(obj);
+#ifndef  __APPLE__
+        auto as_clip = typeinfo_cast<CCClippingNode *>(obj);
         if (as_clip != nullptr) {
             as_clip->setPosition({1000.f, 1000.f});
                 
             continue;
         }
+#endif
     }
 
     // children->release();
